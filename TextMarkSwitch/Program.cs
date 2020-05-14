191745bde671da6dec5ef64a6d5737884148fb0a
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-05-14 14:55:18 +0200

    final draft Text mark exercice using switch

--- TextMarkSwitch/Program.cs
+++ TextMarkSwitch/Program.cs
@@ -8,12 +8,34 @@ namespace TextMarkSwitch
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Enter your mark!");
-            string mark =Console.ReadLine();
+            _ = float.TryParse(Console.ReadLine(), out float mark);
 
+            switch (mark)
+            {
+                case float n when n > 0f && n <= 4f:
+                    Console.WriteLine("Fail");
+                    break;
+                case 5:
+                    Console.WriteLine("Pass");
+                    break;
+                case float n when n >= 6f && n < 7.8f:
+                    Console.WriteLine("Good");
+                    break;
+                case float n when n >= 7.8f && n < 9.10f:
+                    Console.WriteLine("Notable");
+                    break;
 
+                case float n when n <= 9.10f && n <= 100f:
+                    Console.WriteLine("Excellent");
+                    break;
+                default:
+                    Console.WriteLine("Value must be between 0 to 10");
+                    break;
+            }
         }
     }
 }

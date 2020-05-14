f9e46c1c368dc46a4c1de82001d45c006d69957a
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-05-14 14:24:14 +0200

    Add project files.

---
+++ TextMarkSwitch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextMarkSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your mark!");
+            string mark =Console.ReadLine();
+
+
+        }
+    }
+}
